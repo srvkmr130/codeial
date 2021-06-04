907a86ce4e80795db5be83609529d21df5d3761e
srvkmr130@ <srvkmr130@gmail.com>
2021-06-04 07:21:59 +0530

    add noty notification

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 
+//require connect -flas to popup notifications
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
 app.use(sassMiddleware({
     src:'./assets/scss',
     dest:'./assets/css',
@@ -66,6 +70,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router 
 app.use('/',require('./routes'));
